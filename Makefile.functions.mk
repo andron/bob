6f0ab30e92985ecebdaf908b74a4b98a02df77bc
Anders Ro <anders.ronnbrant@gmail.com>
2013-05-25 13:16:30 +0200

    Fixed a bug with reading of indirectly generated .d files
     * Glob-pattern (*.d) is used for finding all .d-files as oppossed to the
       previous solution which only replaced the suffix of the specified source
       files.

--- Makefile.functions.mk
+++ Makefile.functions.mk
@@ -36,7 +36,8 @@ endef
 #
 # $1: Target name -- $2: Target object directory
 define __bob_target_dfiles
-$(wildcard $(addprefix $2,$(addsuffix .d,$(basename $(sort $($1_SRCS))))))
+$(wildcard $(addsuffix *.d,$(addprefix $2,$(sort $(dir $($1_SRCS)))))) \
+$(if $($1_SRCS_RCC),$(wildcard $(addsuffix *.d,$2)))
 endef
 
 # ******************************************************************************
