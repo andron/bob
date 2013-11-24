494fcb8261a4c6c06d0463823ea47c56c49e6eb0
Anders Ro <anders.ronnbrant@gmail.com>
2013-11-24 17:45:46 +0100

    Simplify dirprefix macro
    Removing subst calls makes the macro less complex and faster, the only
    drawback is that some output may contain multiple consecutive '/' which looks
    a bit odd, but it has no effect on the functionality.

--- Makefile.functions.mk
+++ Makefile.functions.mk
@@ -24,12 +24,7 @@
 #
 # $1: Directory name -- $2: Name list to be prepended
 define __bob_dirprefix
-$(call __bob_clean_path,$(if $1,$(addprefix $1/,$2),$(addprefix ./,$2)))
-endef
-
-# Clean a path from multiple //
-define __bob_clean_path
-$(subst /./,/,$(subst //,/,$1))
+$(if $1,$(addprefix $1/,$2),$(addprefix ./,$2))
 endef
 
 # Target .d-files
@@ -40,7 +35,6 @@ $(eval __objdir := $(addprefix $(OBJDIR)/,$(sort $(dir $($1_SRCS))))) \
 $(wildcard $(addsuffix *.d,$(__objdir))) \
 $(if $($1_SRCS_RCC),$(wildcard $(addsuffix *.d,$2)))
 endef
-
 # ******************************************************************************
 
 
